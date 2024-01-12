2222499c8cdbecf1832c7d8bd54d25b75700d89c
Pilar Marinas <pilar.marinas@upcnet.es>
2024-01-12 13:24:24 +0100

    Preparing release 1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='genweb6.patents',
-    version='1.2.dev0',
+    version='1.2',
     description="Genweb 6 package for managing technology offers",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
